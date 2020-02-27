1065a5de228a80300703439d21f4c9fc573b9f75
Badlarry <alex.sinno@gmail.com>
2020-02-27 16:00:12 +0200

    Update bindcajport.ps1

--- bindcajport.ps1
+++ bindcajport.ps1
@@ -6,13 +6,13 @@ $SessionStatus = Get-SessionStatus
     {
       Write-Warning -Message 'Establishing SSH Connection...'
       $creds = Get-Credential -Credential $uname
-      New-SSHSession -ComputerName atl1isensorcaj01.srv.secureworks.net -Credential $creds | Format-List
+      New-SSHSession -ComputerName #(SETJUMPSEVERHERE) -Credential $creds | Format-List
     }
   }
 function Get-SessionStatus
 {
   Get-SSHSession |
-  Where-Object {$_.Host -eq 'atl1isensorcaj01.srv.secureworks.net'} | 
+  Where-Object {$_.Host -eq #(SETJUMPSEVERHERE)} | 
   Select-Object -Property Connected | 
   %{ $_.Connected }
   }
@@ -71,39 +71,4 @@ function Stop-PortFowardConnections {
   %{ $_.BoundPort }
  foreach($p in $bPort)
  {Stop-SSHPortForward -Index 0 -BoundPort $p -BoundHost '127.0.0.1'}
-}
-function Start-MultipleConnections {
-  Connect-ToCAJ $ErrorActionPreference = 'Stop'
-  # DELL CSO1
-  Write-Warning -Message 'Connecting to Dell CSO 1'
-  Connect-ToEndPoint -EndPoint 10.188.31.73  -LocalPort 8081
-  # DELL CSO2
-  Write-Warning -Message 'Connecting to Dell CSO 2'
-  Connect-ToEndPoint -EndPoint 10.188.31.74  -LocalPort 8082
-  # DELL CSO3
-  Write-Warning -Message 'Connecting to Dell CSO 3'
-  Connect-ToEndPoint -EndPoint 10.188.32.111 -LocalPort 8083
-  # KELLOGG
-  Write-Warning -Message 'Connecting to Kellogg'
-  Connect-ToEndPoint -EndPoint 10.188.25.231 -LocalPort 8084
-  # BAIN
-  Write-Warning -Message 'Connecting to Bain'
-  Connect-ToEndPoint -EndPoint 10.188.28.1 -LocalPort 8085
-  # ARAMCO
-  Write-Warning -Message 'Connecting to Aramco'
-  Connect-ToEndPoint -EndPoint 10.188.36.31 -LocalPort 8086
-  # ALLEN & OVERY 1
-  Write-Warning -Message 'Connecting to Allen and Overy 1'
-  Connect-ToEndPoint -EndPoint 10.188.26.87 -LocalPort 8087
-  # Union Bank
-  Write-Warning -Message 'Connecting to Union Bank'
-  Connect-ToEndPoint -EndPoint 10.188.28.75 -LocalPort 8088
-  # New York Jets
-  Connect-ToEndPoint -EndPoint 10.188.31.226 -LocalPort 8089
-  Write-Warning -Message 'Connecting to the New York Jets'
-  # University of Alabama
-  Connect-ToEndPoint -EndPoint 10.188.19.173 -LocalPort 8090
-  Write-Warning -Message 'Connecting to University of Alabama'
-  Write-Warning -Message 'Connections Established'
-  Get-ListOfConnections
 }
